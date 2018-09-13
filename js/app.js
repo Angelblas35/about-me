4d94b9ae3b631d267ba5de3aa45e4d24d7cf5594
Angelblas35 <angelblas35@github.com>
2018-09-12 21:02:03 -0700

    testing before I submit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+ 'use strict';
+
+
+var name = prompt ('Is my name Joey Joe Joe Shabadoo?');
+var response = 'That is a terrible name!';
+console.log(name);
+alert(response);
+
+    
+var pets = prompt ('Do I have any pets?');
+var response = 'Of course I do.';
+console.log(pets);
+alert(response);
+
+var creative = prompt ('Am I being creative with my questions?');
+var response = 'Well, I do believe you are correct.';
+console.log(creative);
+alert(response);
+
+var holiday = prompt ('Is my favorite holiday Halloween?');
+var response = 'BOO!';
+console.log(holiday);
+alert(response);
+
+var all=10;
+var bacons = prompt ('How many bacons I has?');
+var myBacons= 'all the bacons';
+var response = 'Mmmm... Bacon... ahggghhhgghh';
+console.log(bacons);
+alert(response);
+
+var avocado= prompt ('Do I like avocado?');
+var response = 'Doh!';
+console.log(avocado);
+alert(response);
---
+++ js/jsapp.js
@@ -0,0 +1,32 @@
+
+
+var name = prompt ('Is my name Joey Joe Joe Shabadoo?')
+var properName= name.toUpperCase()
+console.log(name);
+alert(name);
+
+    
+
+var where = prompt ('Where was I born>')
+console.log(where);
+alert(where);
+
+var show = prompt ('Now it gets tricky. What is my favorite TV show')
+console.log(show);
+alert(show);
+
+var time = prompt ("What time do I go to sleep")
+console.log(time);
+alert(time);
+
+var bacons = prompt ('How many bacons I has?')
+console.log(bacons);
+alert(bacons);
+
+var dogs= prompt ('Who let the dogs out?')
+console.log(dogs);
+alert(dogs);
+
+
+
+
